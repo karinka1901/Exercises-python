e44cdee204d2d4afe7c36b0ee01408703e1db5aa
karinka1901 <karindomagalska@gmail.com>
2022-08-24 21:33:24 +0300

    ex1

---
+++ exercise week1.py
@@ -0,0 +1,7 @@
+#ex1 print name
+#print('hello, Karin!')
+
+#ex2 ask for a radius of a circle and print its area
+radius = input("enter the radius:")
+area = float(radius) * float(radius) * 3.14
+print(area)
