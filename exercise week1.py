5b642bc00453d2b1f4d48f95bc596ec64ac518c0
karinka1901 <karindomagalska@gmail.com>
2022-08-24 21:54:48 +0300

    , ex2, ex3

--- exercise week1.py
+++ exercise week1.py
@@ -1,7 +1,17 @@
-#ex1 print name
+#("ex1 print your name")
 #print('hello, Karin!')
 
-#ex2 ask for a radius of a circle and print its area
-radius = input("enter the radius:")
-area = float(radius) * float(radius) * 3.14
-print(area)
+#("ex2 ask for a radius of a circle and print its area")
+#radius = input("enter the radius:")
+#area = float(radius)**2 * 3.14
+#print("the area of a circle is " + str(area))
+
+#("ex3 ask for the lenght and width of a rectangle and print the perimeter, area")
+#lenght = input("enter the lenght:")
+#width = input("enter the width:")
+#parameter = (float(lenght) + float(width)) * 2
+#area_rec = float(lenght) * float(width)
+#print("the parameter of the rectangular is " + str(parameter))
+#print("the area of the rectangular is " + str(area_rec))
+
+
