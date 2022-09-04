ebaf1d28af30b1860643d5b3dcd84585ce3c5067
karinka1901 <karindomagalska@gmail.com>
2022-09-04 23:50:30 +0300

    ex1-5 week2 edited

--- ex3-week2.py
+++ ex3-week2.py
@@ -31,16 +31,16 @@ while True:
     if number_float > biggest_num:
         biggest_num = number_float
 
-#ex3 - second method
-print("--------Exercise 3 -Second method--------")
-numlist = []
-while True:
-    number = input("Enter the number: ")
-    if number == "":
-        print("The smallest number is " + str(min(numlist)))
-        print("The biggest number is " + str(max(numlist)))
-        break
-    numlist.append(float(number))
+# #ex3 - second method
+# print("--------Exercise 3 -Second method--------")
+# numlist = []
+# while True:
+#     number = input("Enter the number: ")
+#     if number == "":
+#         print("The smallest number is " + str(min(numlist)))
+#         print("The biggest number is " + str(max(numlist)))
+#         break
+#     numlist.append(float(number))
 
 #ex4
 print("--------Exercise 4--------")
