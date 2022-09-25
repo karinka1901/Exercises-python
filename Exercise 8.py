656a81478602ae2ea84903f282e155f78e18ab10
karinka1901 <karindomagalska@gmail.com>
2022-09-25 19:39:57 +0300

    k

--- Exercise 8.py
+++ Exercise 8.py
@@ -3,7 +3,7 @@
 # Write a program that asks the user to enter the ICAO code of an airport.
 # The program fetches and prints out the corresponding airport name and location (town)
 # from the airport database used on this course.
-# 
+
 
 print("------------------exercise 1--------------------")
 import mysql.connector
@@ -18,17 +18,21 @@ connection = mysql.connector.connect(
          )
 
 def getAirport (ICAO):
-    sql = "SELECT airport.name, country.name FROM airport, country"
+    sql = "SELECT airport.ident, airport.name, country.name FROM airport, country"
     #sql += "WHERE airport.iso_country = country.iso_country and airport.ident='" + ICAO + "'"
-    sql += " WHERE ident='" + ICAO + "'"
-    print(sql)
+    sql += " WHERE airport.id='" + ICAO + "'"
+    #print(sql)
     cursor = connection.cursor()
     cursor.execute(sql)
     result = cursor.fetchall()
     if cursor.rowcount > 0:
         for row in result:
-            print(f"The name of the airport is {row[0]} and it is located in {row[1]}")
+            print(f"The name of the airport is {row[1]} and it is located in {row[2]} the ident is {row[0]}")
             return
 
 ICAO = input("Enter ICAO code: ")
 getAirport(ICAO)
+
+
+#ex2
+print("------------------exercise 2--------------------")
