f6bff86e0f01003162fe5e7398a29e40293baeec
karinka1901 <karindomagalska@gmail.com>
2022-10-18 19:44:30 +0300

    3/4

---
+++ Software 2/Exercises/Exercise10.py
@@ -0,0 +1,168 @@
+#exercise1
+print("---------------exercise1---------------------")
+
+class Elevator:  # type: ignore
+    def __init__(self, bottom_floor, top_floor,):
+        self.top_floor = top_floor
+        self.bottom_floor = bottom_floor
+        self.current_floor = bottom_floor
+    
+    def floor_up(self, current_floor):
+        current_floor += 1
+
+    def floor_down(self, current_floor):
+        current_floor -= 1
+
+    def go_to_floor(self, number):
+        while number != self.current_floor:
+            if number > self.top_floor:
+                print("This floor doesn't exist")
+                break
+            if number < self.bottom_floor:
+                print("This floor doesn't exist")
+                break
+            else:
+                if number > self.current_floor:
+                    self.floor_up(number)
+                    self.current_floor = number
+                    print(f"Floor {self.current_floor}")
+                    break
+                if number < self.current_floor:
+                    self.floor_down(number)
+                    self.current_floor = number
+                    print(f"Floor {self.current_floor}")
+                    break
+        else:
+            print(f'You are already on the {self.current_floor} floor')
+            
+
+e = Elevator(1,7)
+e.go_to_floor(0) # doesn't exist
+e.go_to_floor(8) # doesnt't exist
+e.go_to_floor(7)  
+e.go_to_floor(3)
+e.go_to_floor(3) # already on this floor
+
+
+#exercise2
+print("\n---------------exercise2---------------------")
+class Elevator: # type: ignore
+    def __init__(self, bottom_floor, top_floor,):
+        self.top_floor = top_floor
+        self.bottom_floor = bottom_floor
+        self.current_floor = bottom_floor
+    
+    def floor_up(self, current_floor):
+        current_floor += 1
+
+    def floor_down(self, current_floor):
+        current_floor -= 1
+
+    def go_to_floor(self, number):
+        while number != self.current_floor:
+            if number > self.top_floor:
+                print("This floor doesn't exist")
+                break
+            if number < self.bottom_floor:
+                print("This floor doesn't exist")
+                break
+            else:
+                if number > self.current_floor:
+                    self.floor_up(number)
+                    self.current_floor = number
+                    print(f"Going to floor {self.current_floor}")
+                    break
+                if number < self.current_floor:
+                    self.floor_down(number)
+                    self.current_floor = number
+                    print(f"Going to floor {self.current_floor}")
+                    break
+        else:
+            print(f'You are already on the {self.current_floor} floor')
+
+class Building: # type: ignore
+    def __init__(self, bottom_floor, top_floor, numberof_elevators):
+        self.elevators = []
+        for i in range(numberof_elevators):
+            elevator = Elevator(bottom_floor, top_floor)
+            self.elevators.append(elevator)
+
+    def run_elevator(self, elevator_number, destination_floor):
+        if elevator_number >= 0 and elevator_number < len(self.elevators):
+            self.elevators[elevator_number].go_to_floor(destination_floor)
+            print(f"The elevator number {elevator_number} is on the floor {self.elevators[elevator_number].current_floor}")
+        else:
+            print(f"This elevator {elevator_number} doesn't exist!")
+
+
+        
+#main
+building = Building(0,7,5)
+print(f"There are {len(building.elevators)} elevators in this building")
+building.run_elevator(1,3)
+building.run_elevator(4,7)
+building.run_elevator(5,7)
+
+#exercise3
+print("\n---------------exercise3---------------------")
+class Elevator: 
+    def __init__(self, bottom_floor, top_floor,):
+        self.top_floor = top_floor
+        self.bottom_floor = bottom_floor
+        self.current_floor = bottom_floor
+    
+    def floor_up(self, current_floor):
+        current_floor += 1
+
+    def floor_down(self, current_floor):
+        current_floor -= 1
+
+    def go_to_floor(self, number):
+        while number != self.current_floor:
+            if number > self.top_floor:
+                print("This floor doesn't exist")
+                break
+            if number < self.bottom_floor:
+                print("This floor doesn't exist")
+                break
+            else:
+                if number > self.current_floor:
+                    self.floor_up(number)
+                    self.current_floor = number
+                    break
+                if number < self.current_floor:
+                    self.floor_down(number)
+                    self.current_floor = number
+                    break
+
+class Building:
+    def __init__(self, bottom_floor, top_floor, numberof_elevators):
+        self.elevators = []
+        for i in range(numberof_elevators):
+            elevator = Elevator(bottom_floor, top_floor)
+            self.elevators.append(elevator)
+
+    def run_elevator(self, elevator_number, destination_floor):
+        if elevator_number >= 0 and elevator_number < len(self.elevators):
+            self.elevators[elevator_number].go_to_floor(destination_floor)
+            print(f"The elevator number {elevator_number} is on the floor {self.elevators[elevator_number].current_floor}")
+        else:
+            print(f"This elevator {elevator_number} doesn't exist!")
+
+
+    def fire_alarm(self):
+        print("\nWEEE WOOO FIREEEE!\n")
+        for i in range(len(self.elevators)):
+            self.elevators[i].go_to_floor(self.elevators[i].bottom_floor)
+            print(f"The elevator number {i} is on the floor {self.elevators[i].current_floor}")
+
+building = Building(1,8,6)
+building.run_elevator(0,8)
+building.run_elevator(1,7)
+building.run_elevator(2,5)
+building.run_elevator(3,4)
+building.run_elevator(4,3)
+building.run_elevator(5,6)
+building.run_elevator(6,2)
+
+building.fire_alarm()
