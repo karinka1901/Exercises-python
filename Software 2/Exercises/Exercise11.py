b38bd14272df12d488ec17f2d9488b2a7c044c3e
karinka1901 <karindomagalska@gmail.com>
2022-10-26 10:50:24 +0300

    done

--- Software 2/Exercises/Exercise11.py
+++ Software 2/Exercises/Exercise11.py
@@ -33,3 +33,42 @@ magazine.print_info()
 
 #ex2
 print("---------------------exercise 2-----------------------------")
+
+class Car:
+
+    #km_counter = 0
+
+    def __init__(self, reg_number, max_speed, current_speed = 0, traveled_distance = 0):
+        self.reg_number = reg_number
+        self.max_speed = max_speed
+        self.current_speed = current_speed
+        self.traveled_distance = traveled_distance
+
+    def accelerate (self, speed_change):
+        self.current_speed += speed_change
+        if self.current_speed > self.max_speed:
+            self.current_speed = self.max_speed
+        if self.current_speed < 0:
+            self.current_speed = 0
+    
+    def drive(self, time):
+        self.traveled_distance += time * self.max_speed
+        print(f"{self.reg_number}: {self.traveled_distance} km")
+
+
+class ElectricCar(Car):
+    def __init__(self, reg_number, max_speed, battery_capacity):
+        self.battery_capacity = battery_capacity
+        super().__init__(reg_number, max_speed)
+
+class GasolineCar(Car):
+    def __init__(self, reg_number, max_speed, tank_volume):
+        self.tank_volume = tank_volume
+        super().__init__(reg_number, max_speed)
+
+
+#main
+electric = ElectricCar("ABC-15", 180, 52.5)
+gasoline = GasolineCar("ACD-123", 165, 32.3)
+electric.drive(3)
+gasoline.drive(3)
