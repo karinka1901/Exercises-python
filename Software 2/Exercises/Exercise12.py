a1f0d75865215dbf79f36ad9dda40e5fd5dcdc14
karinka1901 <karindomagalska@gmail.com>
2022-11-09 19:33:43 +0200

    done

--- Software 2/Exercises/Exercise12.py
+++ Software 2/Exercises/Exercise12.py
@@ -1,16 +1,26 @@
-#exercise1
-print("----------------------exercise1----------------------------------")
 import json
 import requests
 
-#keyword = input("Enter keyword: ")
+#exercise1
+print("----------------------exercise1----------------------------------")
 
-# Request template: https://api.tvmaze.com/search/shows?q=girls
-request = "https://api.chucknorris.io/jokes/random"
+response = requests.get("https://api.chucknorris.io/jokes/random").json()
+print(response["value"])
 
-response = requests.get(request)
-if response.status_code==200:
-    json_response = response.json()
-    print(json.dumps(json_response, indent=2))
-    for a in json_response:
-        print(a["value"])
+
+#exercise2
+print("----------------------exercise2----------------------------------")
+
+city = input("Enter the city: ")
+request = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=2553e5564345711d839d4ceb0a760270&units=metric"
+
+try:
+    response = requests.get(request)
+    if response.status_code==200:
+        json_response = response.json()
+        # print(json.dumps(json_response, indent=2))
+        temperature = int(json_response["main"]["temp"])
+        description = json_response["weather"][0]["description"]
+        print(f"Current weather in {city.capitalize()} is {temperature}°C with a {description}.")
+except requests.exceptions.RequestException as e:
+    print ("Request could not be completed.")
