6b7b2e2a4a843974d670cfaea4baa3e8189d41e9
karinka1901 <karindomagalska@gmail.com>
2022-11-24 20:34:00 +0200

    .

--- Software 2/Exercises/Excercise13.py
+++ Software 2/Exercises/Excercise13.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=5000)
 
 
-#exercise2
+# #exercise2
 # app = Flask(__name__)
 # @app.route('/airport/<ICAO>')
 # def airport (ICAO):
